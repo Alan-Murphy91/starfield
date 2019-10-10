212de96a36e0b163cebbf5d0a1d1e1608e9dcac5
Alan <alanjmurphy91@gmail.com>
2019-10-10 23:43:41 +0100

    es6 nd increased speed

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
-const stars = new Array(1000);
+const stars = new Array(800);
 
 function setup(){
     createCanvas(window.screen.availWidth, window.screen.availHeight)
-    for(var i=0; i<1000; i++){
+    for(var i=0; i<800; i++){
         stars[i] = new star();
     }
 }
@@ -10,16 +10,15 @@ function setup(){
 function draw(){
     background(0);
     translate(window.screen.availWidth/2, window.screen.availHeight/2);
-    for(var i=0; i<1000; i++){
+    for(var i=0; i<800; i++){
 		stars[i].show();
-		// console.log(stars[i].x, stars[i].y, stars[i].z);
     }
 }
 
 function star(){
-    this.x = (Math.random() * window.screen.availWidth) + 1;
-    this.y = (Math.random() * window.screen.availHeight) + 1;
-    this.z = (Math.random() * window.screen.availHeight) + 1;
+    this.x = (Math.random() * window.screen.availWidth);
+    this.y = (Math.random() * window.screen.availHeight);
+    this.z = (Math.random() * window.screen.availHeight);
 
     this.x *= Math.random() < 0.5 ? -1 : 1;
     this.y *= Math.random() < 0.5 ? -1 : 1;
@@ -27,14 +26,13 @@ function star(){
     this.show = function(){
         fill(255);
         noStroke();
-        var nx = map(this.x/this.z,0,1,0,window.screen.availWidth/3);
-        var ny = map(this.y/this.z,0,1,0,window.screen.availHeight/3);
-        var size = map(this.z, window.screen.availWidth/2, 1, 0.1,8);
+        const nx = map(this.x/this.z,0,1,0,window.screen.availWidth/3);
+        const ny = map(this.y/this.z,0,1,0,window.screen.availHeight/3);
+        const size = map(this.z, window.screen.availWidth/2, 1, 0.1,8);
 		ellipse(nx,ny,size,size);
-		// console.log(nx, ny);
-		if(nx > window.screen.availWidth || ny > window.screen.availHeight) {
+		if (nx > window.screen.availWidth || ny > window.screen.availHeight) {
 			this.z = window.screen.availWidth/2;
 		}
-        this.z -=1.5;
+        this.z -= 2;
     }
 }
